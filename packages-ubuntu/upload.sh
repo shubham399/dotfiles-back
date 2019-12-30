4557d70edd71868a442c23b97cfde77b4c70c3a9
Shubham Kumar <hello@shubhkumar.in>
2019-12-30 19:28:58 +0530

    Updated backup

--- packages-ubuntu/upload.sh
+++ packages-ubuntu/upload.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 DIR=$(dirname $0)
 cd $DIR
+git add $DIR > /dev/null
+git commit -m "Updated backup" -q
 DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD)
 if [[ $DIFF =~ "packages-ubuntu" ]]; then
-    git add $DIR > /dev/null
-    git commit -m "Updated backup" -q
     git push -f -q
     echo "$0 Updated git"
 else
+    git reset HEAD~1
     echo "$0 Nothing New backed up"
 fi
