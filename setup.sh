f9b79eceb4990cf67c0cff2c0577d94364d6ffa5
Shubham Kumar <shubham.399@gmail.com>
2021-02-03 11:56:00 +0530

    Added rofi Xresources

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ C_PATH=$(pwd)
 # Link wallpaper
 rm $HOME/.wallpaper.png
 ln -s $C_PATH/Wallpapers/wallpaper.png $HOME/.wallpaper.png
-
+ln -s $C_PATH/Xresources $HOME/.Xresources
 rm $HOME/.scripts
 ln -s $C_PATH/scripts $HOME/.scripts
 rm $C_PATH/scripts/monitor.sh
