3401c5d7750a1ffb74b86ab66914ccf9f9ea8f62
Shubham Kumar <hello@shubhkumar.in>
2023-04-13 09:06:23 +0530

    New Stuff

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-sudo apt install zsh tmux curl cargo safeeyes flameshot
-cargo install alacritty
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sudo apt install zsh tmux curl cargo safeeyes flameshot
+# cargo install alacritty
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo apt install i3 i3blocks i3-wm dunst i3lock compton rxvt-unicode xsel  fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior suckless-tools i3status hsetroot rofi xclip -y
+# sudo apt install i3 i3blocks i3-wm dunst i3lock compton rxvt-unicode xsel  fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior suckless-tools i3status hsetroot rofi xclip -y
 DIR=$(dirname $0)
 cd $DIR
 C_PATH=$(pwd)
 # Link wallpaper
-rm $HOME/.wallpaper.png
-ln -s $C_PATH/Wallpapers/wallpaper.png $HOME/.wallpaper.png
-rm $HOME/.Xresources
-ln -s $C_PATH/Xresources $HOME/.Xresources
-rm $HOME/.scripts
-ln -s $C_PATH/scripts $HOME/.scripts
-rm $C_PATH/scripts/monitor.sh
-ln -s $C_PATH/scripts/changeDisplay.sh $C_PATH/scripts/monitor.sh
+# rm $HOME/.wallpaper.png
+# ln -s $C_PATH/Wallpapers/wallpaper.png $HOME/.wallpaper.png
+# rm $HOME/.Xresources
+# ln -s $C_PATH/Xresources $HOME/.Xresources
+# rm $HOME/.scripts
+# ln -s $C_PATH/scripts $HOME/.scripts
+# rm $C_PATH/scripts/monitor.sh
+# ln -s $C_PATH/scripts/changeDisplay.sh $C_PATH/scripts/monitor.sh
 ln -s $C_PATH/home/gitconfig $HOME/.gitconfig
 
 # Link home config files
